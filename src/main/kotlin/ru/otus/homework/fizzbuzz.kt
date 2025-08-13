45dabb8a57e6c9ad052cd24fe5f8ae05273d0f11
YURY TILMAN <uatilman@gmail.com>
2025-08-13 22:37:52 +0300

    Реализовано домашнее задание

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -1,6 +1,13 @@
 package ru.otus.homework
 
-
+// шаг 1 задачи пропущен, так как индексы массива по заданию соответствуют числам на указанных индексах
 fun fizzbuzz(n: Int): Array<String> {
-    TODO("Выполните задание")
+    return Array(n) {
+        when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+    }
 }
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,5 +2,16 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    val numbersSize = numbers.size
+    if (numbersSize < 1) throw IllegalArgumentException("Массив должен содержать хотя бы 2 элемента")
+    numbers.forEachIndexed { index, it ->
+        if (index == numbersSize - 1) throw IllegalArgumentException("Подходящих чисел не найдено")
+        if (it + numbers[index + 1] == target) return intArrayOf(index, index + 1)
+        for (i in index + 1 until numbersSize) {
+            if (it + numbers[i] == target) {
+                return intArrayOf(index, i)
+            }
+        }
+    }
+    throw IllegalArgumentException("Подходящих чисел не найдено")
 }
